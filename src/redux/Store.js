1d7aa99606abfb4b99d27dceca1aa25e1ed94daa
HarithaSarath <138025870+HarithaSarath@users.noreply.github.com>
2023-07-03 16:17:20 +0530

    Add files via upload

---
+++ src/redux/Store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux'
+// import cartItemsReducer from './CartItems'
+import cartItemsReducer from './CartItem'
+
+const store = createStore(cartItemsReducer)
+
+export default store
